a58fd02a147cfe800fa3828e2b46697766a4ede0
ゆうと <yutopia@utopiat.net>
2022-06-05 03:51:39 +0900

    初版

---
+++ Geolocator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace Produire.PGeolocator
+{
+	public class ジオロケーター : IProduireStaticClass
+	{
+		Geolocator geolocator;
+		Geoposition geoposition;
+
+		[自分で]
+		public 位置情報 測位する()
+		{
+			if (geolocator == null) geolocator = new Geolocator();
+			Task<Geoposition> t1 = null;
+			Task.Run(() =>
+			{
+				t1 = geolocator.GetGeopositionAsync().AsTask<Geoposition>();
+			}).Wait();
+
+			try
+			{
+				geoposition = t1.Result;
+			}
+			catch (AggregateException ex)
+			{
+				throw ex.InnerException;
+			}
+			return new 位置情報(geoposition);
+		}
+	}
+	public class 位置情報 : IProduireClass
+	{
+		Geoposition geoposition;
+		public 位置情報(Geoposition geoposition)
+		{
+			this.geoposition = geoposition;
+		}
+		public string 計測方法
+		{
+			get { return geoposition.Coordinate.PositionSource.ToString(); }
+		}
+		public double 精度
+		{
+			get { return geoposition.Coordinate.Accuracy; }
+		}
+		public double 緯度
+		{
+			get { return geoposition.Coordinate.Point.Position.Latitude; }
+		}
+		public double 経度
+		{
+			get { return geoposition.Coordinate.Point.Position.Longitude; }
+		}
+		public double 高度
+		{
+			get { return geoposition.Coordinate.Point.Position.Altitude; }
+		}
+		public string 国
+		{
+			get { return (geoposition.CivicAddress != null) ? geoposition.CivicAddress.Country : null; }
+		}
+		public string 都道府県
+		{
+			get { return (geoposition.CivicAddress != null) ? geoposition.CivicAddress.State : null; }
+		}
+		public string 市町村
+		{
+			get { return (geoposition.CivicAddress != null) ? geoposition.CivicAddress.City : null; }
+		}
+		public string 郵便番号
+		{
+			get { return (geoposition.CivicAddress != null) ? geoposition.CivicAddress.PostalCode : null; }
+		}
+		public DateTimeOffset 測定時刻
+		{
+			get { return geoposition.Coordinate.Timestamp; }
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Produire.Geolocator
-プロデルジオロケータープラグインでは、測位した位置情報を取得できます。
+# プロデルジオロケータープラグイン
+測位した位置情報を取得するためのプロデルプラグインです。
+位置情報は、GPSなどの測位機器によって取得する他に、Wi-Fiの情報を用いて算出されます。
+
+## 動作対象
+プロデル 1.8.1137
+Windows 10 Build 19041
+
+## 利用方法
+プロデルがあるpluginsフォルダにProduire.Geolocator.dllをコピーするか
+プログラム冒頭に
+```
+「Produire.Geolocator.dll」を利用する
+```
+と指定します。
+
+Copyright (c) 2022 utopiat.net
